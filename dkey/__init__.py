0cae2af8b50fc54c972bfa2d0a2e2afd75918f88
NOhs <NOhs@users.noreply.github.com>
2019-03-30 15:01:31 +0100

    Added version information to the module via pbr.

--- dkey/__init__.py
+++ dkey/__init__.py
@@ -10,6 +10,20 @@ dkey
 ====
 Function to generate deprecated keys.
 
+__version__
+===========
+A string indicating which version of dkey is currently used.
+
+version_info
+============
+A tuple containing the currently used version.
+
 """
 from ._dkey import deprecate_keys as deprecate_keys
 from ._dkey import dkey as dkey
+
+from pbr.version import VersionInfo
+
+_v = VersionInfo('mgen').semantic_version()
+__version__ = _v.release_string()
+version_info = _v.version_tuple()
